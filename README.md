3fc8d6020d50840dd241182801e43dd7db9f4dea
Ted Naleid <contact@naleid.com>
2018-02-28 21:17:51 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## compile/install
+
+    nocorrect go fmt && golint && go test ./... && go install
+
+# test GET with `ganda`
+
+    time seq 10000 | awk '{printf "http://localhost:1323/%s\n", $1}' | ganda -s
+
+# test POST with `ganda` using body
+
+    time seq 10000 | awk '{printf "http://localhost:1323/%s %s\n", $1, $1}' | ganda -s -X POST -d '{"id": "%s"}'
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.BodyDump(func(c echo.Context, reqBody, resBody []byte) {}))
+	e.Use(middleware.Recover())
+
+	e.GET("/:id", getResult)
+	e.POST("/:id", postResult)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getResult(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+func postResult(c echo.Context) error {
+	if c.Request().Body != nil {
+		reqBody, _ := ioutil.ReadAll(c.Request().Body)
+		reqString := string(reqBody)
+
+		if len(reqString) > 0 {
+			return c.String(http.StatusOK, reqString)
+		}
+	}
+
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
